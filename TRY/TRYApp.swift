ac3e027ac618ded6af88d242a1da8539e71ae80a
Alon Ribovsky <alonri75@icloud.com>
2023-06-24 06:31:57 +0300

    Initial Commit

---
+++ TRY/TRYApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRYApp.swift
+//  TRY
+//
+//  Created by Alon Ribovsky on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TRYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
